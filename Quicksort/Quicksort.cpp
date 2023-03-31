8df25319d852e4683dd4e5815bd6fae2267e3101
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-03-31 15:59:51 +0700

    Membuat prosedur q_short dengan tipe void dan parameter int low , int hight serta terdapat variabel pivot, i, j. lalu terdapat prosedur if untuk membandingkan nilai

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -39,4 +39,19 @@ void swap(int x, int y)
 	arr[y] = temp;
 }
 
+void q_short(int low, int high)
+{
+	int pivot, i, j;
+	if (low > high) //langkah 1
+		return;
 
+	//Partition the list into two parts:
+	//one containing elements less that or equal to pivot
+	//Outher containing elements greather than pivot
+
+	pivot = arr[low]; //langkah 2
+
+	i = low + 1; //langkah 3
+	j = high; //langkah 4
+
+	
