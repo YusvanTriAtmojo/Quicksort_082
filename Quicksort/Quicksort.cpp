bb837b199074555b64dfc43ec3625849680e2c77
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-03-31 15:53:32 +0700

    Membuat perulangan while untuk memasukkan panjang array

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -8,4 +8,19 @@ int mov_count = 0; //number of data movement
 int n;
 
 void input() {
+	while (true)
+	{
+		cout << "Masukan panjang element array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMasukan panjang array adalah 0" << endl;
+	}
+
+	cout << "\n===================" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n===================" << endl;
+
 	
